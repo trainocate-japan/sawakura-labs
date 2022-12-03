5199ad5d04f949d549eaa11e123f994ebb2a8a8a
JinKanai <jin@kanaisan.com>
2022-12-03 10:52:20 +0900

    fix setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -9,14 +9,15 @@ create table IF not exists contents
 (
 id               INT(20) AUTO_INCREMENT PRIMARY KEY,
 course_code             VARCHAR(16) NOT NULL,
-course_name             VARCHAR(120) NOT NULL
+course_name             VARCHAR(120) NOT NULL,
+duration                SMAILLINT NOT NULL
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 ---- insert data ----
-INSERT INTO contents (id, course_code, course_name)
+INSERT INTO contents (id, course_code, course_name, duration)
 VALUES
-(0, 'NFC0313G', 'クラウドコンピューティング概要'),
-(0, 'NFC0314G', 'ビジネスパーソンのためのクラウド入門'),
-(0, 'NFC0267G', 'クラウドアーキテクト・ファーストステップ'),
-(0, 'NFC0295G', '実践クラウドデザインパターン'),
-(0, 'NFC0477G', 'さわってわかるクラウド入門');
+(0, 'NFC0313G', 'クラウドコンピューティング概要', 1),
+(0, 'NFC0314G', 'ビジネスパーソンのためのクラウド入門', 1),
+(0, 'NFC0267G', 'クラウドアーキテクト・ファーストステップ', 2),
+(0, 'NFC0295G', '実践クラウドデザインパターン', 2),
+(0, 'NFC0477G', 'さわってわかるクラウド入門', 1);
